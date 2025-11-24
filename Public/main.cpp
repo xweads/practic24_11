80c4623ca69a8208db683a919dfb24a7141fcfed
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2025-11-24 13:05:46 +0300

    draw free make mostleft

--- Public/main.cpp
+++ Public/main.cpp
@@ -29,9 +29,30 @@ int main(){
     }
     draw(mostleft(pls,k));
     free_planars(pls,k);
-    // pls[k++]=make(std::cin)
     Planar * left=mostleft(pls,k);
-    // draw(left);
-    // free_planars(pls,k);
+    
+    Planar * make(size_t pl){
+        switch (pl)
+        {
+            default;
+                throw std::logic_error("bad id");
+        }
+        return nullptr;
+    }
+    
+    void free_planars(Planar ** pls, size_t k){
+        for(size_t i=0; i<k; ++i);
+        delete pls[i];
+    }
+    
+    void draw( Planar * pl){
+        std::cout<<pl->x();
+        std::cout<<pl->y()<<"\n";
+        
+    }
+    
+    Planar *mostleft(Planar ** pls, size_t k){
+        return nullptr;
+    }
     
 }
