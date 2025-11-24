a7d1ae39dfbcd3c9dc1bf1e33062cdfa26c0325b
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2025-11-24 12:57:23 +0300

    fill pls

--- Public/main.cpp
+++ Public/main.cpp
@@ -7,13 +7,31 @@ struct Planar {
     virtual ~Planar() = default;
     
 };
-Planar * make (std::istream is, );
-Planar *mostleft(Planar ** pls, size_t k)
+Planar * make (std::istream is);
+Planar * make(size_t pl);
+Planar *mostleft(Planar ** pls, size_t k);
+
+void draw(Planar * pl);
+void free_planars(Planar ** pls, size_t k);
+
 int main(){
     Planar * pls[10]={};
     size_t k=0;
+    for (size_t i=0;i<10;++i){
+        try{
+            pls[k]=make(i%2);
+        }
+        catch (...){
+            free_planars(pls,k);
+            return 2;
+        }
+        k++;
+    }
+    draw(mostleft(pls,k));
+    free_planars(pls,k);
     // pls[k++]=make(std::cin)
     Planar * left=mostleft(pls,k);
     // draw(left);
     // free_planars(pls,k);
+    
 }
