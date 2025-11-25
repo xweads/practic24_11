c82119f8434d1542d7e17ba58414fa97c38ec1ad
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2025-11-25 20:03:40 +0300

    method Vector

--- Public/main.cpp
+++ Public/main.cpp
@@ -95,5 +95,20 @@ int main(){
         return x() * x () + y() *y();
     }
     
+    int Vector::x() const
+    {
+      return data[1].x() - data[0].x();
+    }
+
+    int Vector::y() const
+    {
+      return data[1].y() - data[0].y();
+    }
+
+    int Vector::abs_sqr() const
+    {
+      return x() * x() + y() * y();
+    }
+    
     
 }
